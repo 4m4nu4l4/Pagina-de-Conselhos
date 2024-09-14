50190a7c73a7a5212dd1bcdceb1b223e53417a4d
Maria <mariaeduardabenevenutti77@gmail.com>
2024-09-13 21:05:23 -0300

    feat: done diario page

--- src/pages/Card-Aleatorio/index.jsx
+++ src/pages/Card-Aleatorio/index.jsx
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-// Função que retorna o componente CardAleatorio
 export default function CardAleatorio() {
-  // Estado para armazenar a frase aleatória
   const [frase, setFrase] = useState("");
 
-  // Função para buscar a frase da API
   const buscarFrase = async () => {
     try {
-      const response = await fetch("https://api.adviceslip.com/advice"); // Exemplo de API que retorna conselhos
+      const response = await fetch("https://api.adviceslip.com/advice"); 
       const data = await response.json();
-      setFrase(data.slip.advice); // Atualiza o estado com a frase recebida
+      setFrase(data.slip.advice); 
     } catch (error) {
       console.error("Erro ao buscar a frase", error);
     }
   };
-
-  // Hook para buscar uma frase aleatória ao carregar a página
+  
   useEffect(() => {
     buscarFrase();
   }, []);
--- src/pages/Card-Diario/index.jsx
+++ src/pages/Card-Diario/index.jsx
@@ -1,12 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import wish from "../../assets/imgs/wish.png";
- 
+import img01 from "../../assets/random-img/card-inicial/verde.jpg"
+import img02 from "../../assets/random-img/card-inicial/green.jpg"
+import img03 from "../../assets/random-img/card-inicial/arvore.jpg"
+import img04 from "../../assets/random-img/card-inicial/aguia.jpg"
+// import img05 from "../../assets/random-img/card-inicial/arvore.jpg"
+import img05 from "../../assets/random-img/card-inicial/balloons.jpg"
+import img06 from "../../assets/random-img/card-inicial/hands.jpg"
+import img07 from "../../assets/random-img/card-inicial/lake.jpg"
+import img08 from "../../assets/random-img/card-inicial/sunset.jpg"
+import img09 from "../../assets/random-img/card-inicial/woman.jpg"
+
+const random_img = [
+  img01,
+  img02, 
+  img03,
+  img04,
+  img05,
+  img06,
+  img07,
+  img08,
+  img09
+]
 export default function CardDiario() {
   const [conselho, setConselho] = useState("");
   const [dataAtual, setDataAtual] = useState("");
+  const [imagensAtuais, setImagensAtuais] = useState([]);
   
   useEffect(() => {
+    const selecionarImagens = () => {
+      const imagensAleatorias = Array(4) 
+        .fill()
+        .map(() => {
+          const indice = Math.floor(Math.random() * random_img.length);
+          return random_img[indice];
+        });
+      setImagensAtuais(imagensAleatorias);
+    };
+    selecionarImagens();
     const fetchConselho = async () => {
       try {
         const response = await fetch("https://api.adviceslip.com/advice");
@@ -43,6 +75,16 @@ export default function CardDiario() {
         </strong>
         <h2>Conselho do Dia</h2>
         <p>{conselho ? conselho : "Carregando conselho..."}</p>
+      </div>
+      <div id="display-imgs">
+        {imagensAtuais.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            style={{ width: 'auto', height: '200px', borderRadius: '15px' }}
+            alt={`imagem-${index}`}
+          />
+        ))}
       </div>
     </div>
   );
