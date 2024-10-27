2d6cd15f7e841de6bab61631a49889104693e43c
Manu <emanuelevpries5252@gmail.com>
2024-10-27 14:37:17 -0300

    Fix: Navbar, cadastro e login

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,68 +1,91 @@
 import "./styles.css";
-import { Link, useLocation } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../../auth/Context';
+import { Link, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../auth/Context";
 
 export default function Header() {
-
   const { token, role } = useContext(AuthContext);
   const location = useLocation();
 
-  const isLoginRoute = location.pathname === '/login';
+  const isLoginRoute = location.pathname === "/login";
 
   return (
     <>
       <div id="header">
         <div>
-        {
-          token &&<Link to="/login" className="menu-link" style={{textDecoration: 'none'}}><p id="title">Wish Daily</p></Link>
-        }
+          {token && (
+            <Link
+              to="/sobre"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            >
+              <p id="title">Wish Daily</p>
+            </Link>
+          )}
         </div>
         <div id="header-pages">
-        <div>
-            {
-              token &&<Link to="/" className="menu-link" style={{textDecoration: 'none'}}><p id="cadastrar-regra">Diário</p></Link>
-            }
-          </div>
+          {token && (
+            <Link
+              to="/diario"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            >
+              <p id="cadastrar-regra">Diário</p>
+            </Link>
+          )}
 
           {/* <Link to="/categoria">
             <p  className="pages">Categoria</p>
           </Link> */}
 
-          <div>
-            {
-              
-              token && <Link to="/conselho" className="menu-link" style={{textDecoration: 'none'}}><p id="cadastrar-regra">Aleatório</p></Link>
-            }
-          </div>
+          {token && (
+            <Link
+              to="/aleatorio"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            >
+              <p id="cadastrar-regra">Aleatório</p>
+            </Link>
+          )}
 
-          <div>
-            {
-              token && <Link to="/conselho" className="menu-link" style={{textDecoration: 'none'}}><p id="cadastrar-regra">Mensal</p></Link>
-            }
-          </div>
+          {token && (
+            <Link
+              to="/mensal"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            >
+              <p id="cadastrar-regra">Mensal</p>
+            </Link>
+          )}
 
-          <div>
-            {
-              !isLoginRoute && !token
-                ? <Link to="/conselho" className="menu-link" style={{textDecoration: 'none'}}><p id="cadastrar-regra">Deixe seu conselho</p> </Link>
-                : null
-            }
-          </div>
+          {!isLoginRoute && !token ? (
+            <Link
+              to="/conselho"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            >
+              <p id="cadastrar-regra">Deixe seu conselho</p>{" "}
+            </Link>
+          ) : null}
 
-          <div>
-            {
-              !isLoginRoute && !token
-                ? <Link to="/login" className="menu-link" style={{textDecoration: 'none'}}></Link>
-                : null
-            }
-          </div>
+          {!isLoginRoute && !token ? (
+            <Link
+              to="/login"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            ></Link>
+          ) : null}
 
-          <div>
-            {
-              token && <Link to="/sobre" className="menu-link" style={{textDecoration: 'none'}}> <p id="cadastrar-regra">Sobre o projeto</p> </Link>
-            }
-          </div>
+          {token && (
+            <Link
+              to="/sobre"
+              className="menu-link"
+              style={{ textDecoration: "none" }}
+            >
+              {" "}
+              <p id="cadastrar-regra">Sobre o projeto</p>{" "}
+            </Link>
+          )}
         </div>
       </div>
     </>
